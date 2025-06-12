f41da244535e8ad7aea675ad82a3da18d2294028
GroovyGab <ghernandezlohaus.78@gmail.com>
2025-06-11 20:32:00 -0400

    Misceláneo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './lib/setup';
 import { LogLevel, SapphireClient } from '@sapphire/framework';
 import { IntentsBitField } from 'discord.js';
+import { PrismaClient } from '../generated';
+import { MetroAPI } from './lib/metro/MetroAPI';
 
 const client = new SapphireClient({
 	defaultPrefix: 'm!',
@@ -13,14 +15,22 @@ const client = new SapphireClient({
 	],
 	logger: {
 		level: LogLevel.Debug
-	}
+	},
+	loadMessageCommandListeners: true
 });
+
+client.prisma = new PrismaClient();
+client.metro = new MetroAPI();
 
 const main = async () => {
 	try {
 		client.logger.info('Iniciando sesión...');
 		await client.login();
 		client.logger.info('Inicio de sesión exitoso!');
+
+		client.logger.info('[DB] Conectando a la base de datos...');
+		await client.prisma.$connect();
+		client.logger.info('[DB] Conexión a la base de datos exitosa!');
 	} catch (error) {
 		client.logger.fatal(error);
 		await client.destroy();
@@ -29,3 +39,10 @@ const main = async () => {
 };
 
 void main();
+
+declare module 'discord.js' {
+	interface Client {
+		prisma: PrismaClient;
+		metro: MetroAPI;
+	}
+}
