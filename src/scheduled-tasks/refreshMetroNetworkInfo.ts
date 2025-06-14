762c1430fb44e6a5c8ff2719bce145805afd1fb2
GroovyGab <ghernandezlohaus.78@gmail.com>
2025-06-13 22:47:22 -0400

    agregar tarea para refrescar el estado de la red

--- src/scheduled-tasks/refreshMetroNetworkInfo.ts
+++ src/scheduled-tasks/refreshMetroNetworkInfo.ts
@@ -5,7 +5,29 @@ import { ScheduledTask } from '@sapphire/plugin-scheduled-tasks';
 	interval: 60_000
 })
 export class UserTask extends ScheduledTask {
-	public run() {
-		this.container.logger.info('This task runs every minute');
+	public async run() {
+		const updates = await this.container.prisma.metroStatusMessage.findMany();
+
+		for (const update of updates) {
+			const currentInfoHash = await this.container.metro.getNetworkStatusHash();
+
+			if (update.api_data_hash === currentInfoHash) return;
+
+			const guild = this.container.client.guilds.cache.get(update.guild_id);
+
+			if (!guild) return;
+
+			const channel = guild.channels.cache.get(update.channel_id);
+
+			if (!channel) return;
+
+			if (!channel.isTextBased()) return;
+
+			const message = channel.messages.cache.get(update.message_id);
+
+			if (!message) return;
+
+			message.edit(`Mensaje editado: ${new Date()}`);
+		}
 	}
 }
